9130465c368bfb3f11809eda3db105dabaae9f06
fpauxberger <fpauxberger@pivotal.io>
2019-07-02 17:04:21 +0200

    Fix shell script syntax issue

--- scripts/pave_iaas.sh
+++ scripts/pave_iaas.sh
@@ -6,22 +6,22 @@ echo "Let's create the TF variables file"
 echo
 
 cat <<EOF > tf_iaas/terraforming-pas/terraform.tfvars
-env_name           = “farb”
-access_key         = “$ACCESS_KEY”
-secret_key         = “$SECRET_KEY”
-region             = “eu-central-1"
-availability_zones = [“eu-central-1a”, “eu-central-1b”, “eu-central-1c”]
+env_name           = "farb"
+access_key         = "$ACCESS_KEY"
+secret_key         = "$SECRET_KEY"
+region             = "eu-central-1"
+availability_zones = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
 rds_instance_count = 1
-dns_suffix         = “whizzosoftware.com”
-hosted_zone        = “farb.whizzosoftware.com”
-vpc_cidr           = “10.0.0.0/16"
-ops_manager_ami    = “ami-08a1a17cbd9d0fbf9”
+dns_suffix         = "whizzosoftware.com"
+hosted_zone        = "farb.whizzosoftware.com"
+vpc_cidr           = "10.0.0.0/16"
+ops_manager_ami    = "ami-08a1a17cbd9d0fbf9"
 use_route53        = true
 use_tcp_routes     = true
 
 tags = {
-   Team = “Dev”
-   Project = “WebApp3”
+   Team = "Dev"
+   Project = "WebApp3"
 }
 EOF
 
