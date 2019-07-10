cb45655eb40146bbc8c086cb32c91408b08b6d2a
pvtl-dnoguerol <dnoguerol@pivotal.io>
2019-07-10 15:57:11 +0200

    Added task to create opsman config.

---
+++ scripts/create_opsman_config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+find .
+REGION_NAME=`cat terraform-output/terraform.1.0.0.out | jq -r '.modules[0].outputs.region.value'`
+VPCSUBNET_ID=`cat terraform-output/terraform.1.0.0.out | jq -r '.modules[0].outputs.management_subnet_ids.value[0]'`
+SG_ID=`cat terraform-output/terraform.1.0.0.out | jq -r '.modules[0].outputs.ops_manager_security_group_id.value'`
+KEYPAIR_NAME=`cat terraform-output/terraform.1.0.0.out | jq -r '.modules[0].outputs.ops_manager_ssh_public_key_name.value'`
+INSTANCEPROFILE_NAME=`cat terraform-output/terraform.1.0.0.out | jq -r '.modules[0].outputs.ops_manager_iam_instance_profile_name.value'`
+cat <<EOF > config/opsman.yml
+---
+opsman-configuration:
+  aws:
+    region: $REGION_NAME
+    vm_name: ops-manager-foundation
+    vpc_subnet_id: $VPCSUBNET_ID
+    security_group_id: $SG_ID
+    key_pair_name: $KEYPAIR_NAME
+    private_ip: 10.0.16.2
+    use_instance_profile: true
+    iam_instance_profile_name: $INSTANCEPROFILE_NAME
+
+EOF
